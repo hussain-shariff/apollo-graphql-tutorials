342631c46eeab6b08f538db4f6d582e3ab9414e1
hussain-shariff <hussainowaiz070@gmail.com>
2023-05-23 14:14:14 +0530

    graphql install

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express")
 const app = express()
 require("dotenv").config
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schemas/schema')
 
 const PORT = process.env.PORT || 5000
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql : process.env.NODE_ENV === "development"
+}))
 
 app.get('/', (req, res)=>{
     res.send('hello world')
